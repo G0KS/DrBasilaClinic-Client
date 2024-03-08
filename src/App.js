5960a2bceb1c3b493d05b88c12260ec2558c7ca8
G0KS <goksrofficial@gmail.com>
2024-03-08 11:00:31 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Home from "./Pages/Home";
+import Navbar from "./Components/Navbar";
+import About from "./Pages/About";
+import Services from "./Pages/Services";
+import Contact from "./Pages/Contact";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+   return (
+      <div className="App">
+         <Navbar />
+         <Routes>
+            <Route path="/" Component={Home} />
+            <Route path="/about" Component={About} />
+            <Route path="/services" Component={Services} />
+            <Route path="/contact" Component={Contact} />
+         </Routes>
+      </div>
+   );
 }
 
 export default App;
---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+   <div>
+        <div>Navbar
+        </div>
+        <button><a href='/about'>about</a></button>
+   </div>
+  )
+}
+
+export default Navbar
