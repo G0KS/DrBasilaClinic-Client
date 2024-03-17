61a78ae37e36160d08c39cfd5795a3454b375069
G0KS <goksrofficial@gmail.com>
2024-03-17 11:16:23 +0530

    hero section completed

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,4 +1,4 @@
-import homeImage from "../../assets/homeImage.jpg";
+import heroImage from "../../assets/homeImage.jpg";
 
 import React from "react";
 
@@ -7,27 +7,25 @@ import "./Home.css";
 function Home() {
    return (
       <>
-         <div className="homeContainer pt-5">
-            <div className="container d-flex gap-5 align-items-center">
-               <div className="homeImg text-center">
-                  <img src={homeImage} alt="Home Image" />
-               </div>
-               <div className="homeContent text-center">
-                  <h1>Better Life Through Better Treatment</h1>
-                  <p>
-                     Homeopathic treatment is ideal to optimize the body's
-                     natural defenses gainst microbes. With the appropriate
-                     homeopathic treatment, people recover from infectious
-                     diseases, even the most serious ones gently and rapidly.
-                  </p>
-                  <p>
+         <section className="heroContainer">
+            <div className="container py-5 d-flex gap-5 align-items-center">
+               <div className="heroContent text-center d-flex align-items-center">
+                  <h2>
                      Homeopathy is safest with more reliable approach for
                      ailments and it has withstood the assaults of established
                      medical practice for over 100 years
-                  </p>
+                  </h2>
+               </div>
+               <div className="heroImg text-center">
+                  <img src={heroImage} alt="Hero" />
                </div>
             </div>
-         </div>
+            <div className="banner">
+               <h1 className="text-center">
+                  Better Life Through Better Treatment
+               </h1>
+            </div>
+         </section>
       </>
    );
 }
